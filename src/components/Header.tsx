ca87e20ff021bcef8455c6c5a9540b2bc303606e
tomarvansh475-arch <tomarvansh475@gmail.com>
2026-06-01 20:33:54 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import NgoLogo from "./NgoLogo";
-import { dbInstance } from "../lib/db";
+import { dbInstance } from "../lib/db.ts";
 
 interface HeaderProps {
   activeSection: string;
